289544f6be3d875d7da387b7e01ae253a1a7f895
Esperança Pinoca <esperancapinocaof@gmail.com>
2026-03-12 22:44:58 +0200

    fix: use /tmp for sqlite on vercel

--- config/settings.py
+++ config/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/tmp/db.sqlite3' if 'vercel' in os.environ.get('VERCEL_URL', '') or 'VERCEL' in os.environ else BASE_DIR / 'db.sqlite3',
     }
 }
 
