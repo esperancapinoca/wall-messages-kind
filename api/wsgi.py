007eae3a423c30c0feb3ce335c9a401e5eaed406
Esperança Pinoca <esperancapinocaof@gmail.com>
2026-03-12 22:48:46 +0200

    Auto-migrate database on startup

--- api/wsgi.py
+++ api/wsgi.py
@@ -13,3 +13,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 app = get_wsgi_application()
+
+# --- ADICIONE ISTO AQUI EM BAIXO ---
+from django.core.management import call_command
+try:
+    # Isto cria as tabelas na pasta /tmp do Vercel assim que o site liga
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Erro na migração: {e}")
